c2035158fdf449f75ca1808b0c926f3cb288609b
André <ruland@outlook.de>
2020-06-16 10:45:05 +0200

    TPL ist erstmal nur Parallel

---
+++ HalloTPL/HalloTPL/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HalloTPL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Hallo TPL ***");
+
+            Parallel.For(0, 100000, i => Console.WriteLine(i));
+            //Parallel.Invoke(Zähle, Zähle, Zähle, () => { ZeigeText("lala"); }, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle, Zähle);
+            var texte = new List<string>();
+            foreach (var item in texte.Where(x => x.StartsWith("b")).AsParallel())
+            { }
+
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+
+        private static void Zähle()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: {i}");
+            }
+        }
+
+        static void ZeigeText(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+    }
+}
